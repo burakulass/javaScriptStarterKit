87308b9267f408ec0cc641ec6856b006a159676a
burakulas05 <bulas8193@gmail.com>
2021-11-29 18:46:11 +0300

    engin demirog 18 ekim ders sonu, for döngüsü burda

--- src/app.js
+++ src/app.js
@@ -1,36 +1,24 @@
 console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
-
 let dolarDun = 9.20
-dolarDun = "9.20"
-{
-    let dolarDun = 9.10
-}
+let dolarBugun = 9.40
+console.log(dolarBugun)
 
-console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
+// var kullanmıyoruz çünkü eski ve değiştirmek istediğimizde değişmiyor
+// const ise sabit kalacak değişkenler için kullanıyoruz yani nadir kullanılır
+// let ise bize en uygun değişken metodudur.
+// const eurodun= 15
+// const eurobugun= 15.5
+// const eurodun=14.98
 
-console.log(euroDun)
+// console.log(eurodun) // hata verdi
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+let konutKredileri = ["Konut Kredisi","Mağaza Kredisi","Araç Kredisi","Eğitim kredisi"]
+console.log(konutKredileri)
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let i=0 ;i<konutKredileri.length;i++){
+ console.log("<li> "+konutKredileri[i] +" </li>")
 }
 console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
